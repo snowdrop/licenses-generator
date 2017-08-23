5e9743891f67d2efcbff44e4bdee511a701bb0d8
Gytis Trikleris <gytis@redhat.com>
2017-08-23 14:30:00 +0200

    Add args support to Generator.main method

--- src/main/java/org/jboss/snowdrop/licenses/Generator.java
+++ src/main/java/org/jboss/snowdrop/licenses/Generator.java
@@ -18,20 +18,43 @@ package org.jboss.snowdrop.licenses;
 
 import org.jboss.snowdrop.licenses.xml.LicenseSummary;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Properties;
+
 /**
  * @author <a href="mailto:gytis@redhat.com">Gytis Trikleris</a>
  */
 public class Generator {
 
     public static void main(String... args) throws Exception {
+        Properties properties = argsToProperties(args);
         LicenseSummaryFactory licenseSummaryFactory = new LicenseSummaryFactory();
         LicenseFilesManager licenseFilesManager = new LicenseFilesManager();
-        // To test with *-redhat dependencies you need to change repository in application.properties
-//        LicenseSummary licenseSummary =
-//                licenseSummaryFactory.getLicenseSummary("org.jboss.snowdrop", "spring-boot-1.4-bom", "2.0.0.redhat-28");
-        LicenseSummary licenseSummary = licenseSummaryFactory.getLicenseSummary("junit", "junit", "4.12");
-        licenseFilesManager.createLicensesXml(licenseSummary, "target/licenses");
-        licenseFilesManager.createLicensesHtml(licenseSummary, "target/licenses");
+
+        LicenseSummary licenseSummary = licenseSummaryFactory.getLicenseSummary(properties.getProperty("groupId"),
+                properties.getProperty("artifactId"), properties.getProperty("version"),
+                properties.getProperty("type", "jar"));
+
+        licenseFilesManager.createLicensesXml(licenseSummary, properties.getProperty("destination"));
+        licenseFilesManager.createLicensesHtml(licenseSummary, properties.getProperty("destination"));
+    }
+
+    private static Properties argsToProperties(String... args) {
+        Properties properties = new Properties();
+        Arrays.stream(args)
+                .map(s -> s.replace("-D", ""))
+                .filter(s -> s.contains("="))
+                .map(s -> s.split("="))
+                .filter(a -> a.length == 2)
+                .forEach(a -> properties.put(a[0], a[1]));
+
+        Objects.requireNonNull(properties.getProperty("groupId"), "'groupId' is required");
+        Objects.requireNonNull(properties.getProperty("artifactId"), "'artifactId' is required");
+        Objects.requireNonNull(properties.getProperty("version"), "'version' is required");
+        Objects.requireNonNull(properties.getProperty("destination"),  "'destination' is required");
+
+        return properties;
     }
 
 }
