bd250fa5e8579aaae922d367d14bf7f59d898e2e
Ladislav Thon <lthon@redhat.com>
2019-05-15 15:33:22 +0200

    add ability to specify version-range or version-regexp in license-exceptions.json

---
+++ src/test/java/me/snowdrop/licenses/sanitiser/exceptions/RangeVersionMatcherTest.java
@@ -0,0 +1,63 @@
+package me.snowdrop.licenses.sanitiser.exceptions;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RangeVersionMatcherTest {
+    @Test
+    public void singleVersion() throws Exception {
+        VersionMatcher matcher = new RangeVersionMatcher("1.0.0");
+        assertThat(matcher.matches("1.0.0")).isTrue();
+        assertThat(matcher.matches("1.0.0-redhat")).isFalse();
+        assertThat(matcher.matches("2.0.0")).isFalse();
+    }
+
+    @Test
+    public void lowerBoundInclusive_upperBoundInclusive() throws Exception {
+        VersionMatcher matcher = new RangeVersionMatcher("[1.0.0, 2.0.0]");
+        assertThat(matcher.matches("0.5.0")).isFalse();
+        assertThat(matcher.matches("1.0.0")).isTrue();
+        assertThat(matcher.matches("1.0.0-redhat")).isTrue();
+        assertThat(matcher.matches("1.5.0")).isTrue();
+        assertThat(matcher.matches("2.0.0")).isTrue();
+        assertThat(matcher.matches("2.0.0-redhat")).isFalse();
+        assertThat(matcher.matches("2.5.0")).isFalse();
+    }
+
+    @Test
+    public void lowerBoundExclusive_upperBoundInclusive() throws Exception {
+        VersionMatcher matcher = new RangeVersionMatcher("(1.0.0, 2.0.0]");
+        assertThat(matcher.matches("0.5.0")).isFalse();
+        assertThat(matcher.matches("1.0.0")).isFalse();
+        assertThat(matcher.matches("1.0.0-redhat")).isTrue();
+        assertThat(matcher.matches("1.5.0")).isTrue();
+        assertThat(matcher.matches("2.0.0")).isTrue();
+        assertThat(matcher.matches("2.0.0-redhat")).isFalse();
+        assertThat(matcher.matches("2.5.0")).isFalse();
+    }
+
+    @Test
+    public void lowerBoundInclusive_upperBoundExclusive() throws Exception {
+        VersionMatcher matcher = new RangeVersionMatcher("[1.0.0, 2.0.0)");
+        assertThat(matcher.matches("0.5.0")).isFalse();
+        assertThat(matcher.matches("1.0.0")).isTrue();
+        assertThat(matcher.matches("1.0.0-redhat")).isTrue();
+        assertThat(matcher.matches("1.5.0")).isTrue();
+        assertThat(matcher.matches("2.0.0")).isFalse();
+        assertThat(matcher.matches("2.0.0-redhat")).isFalse();
+        assertThat(matcher.matches("2.5.0")).isFalse();
+    }
+
+    @Test
+    public void lowerBoundExclusive_upperBoundExclusive() throws Exception {
+        VersionMatcher matcher = new RangeVersionMatcher("(1.0.0, 2.0.0)");
+        assertThat(matcher.matches("0.5.0")).isFalse();
+        assertThat(matcher.matches("1.0.0")).isFalse();
+        assertThat(matcher.matches("1.0.0-redhat")).isTrue();
+        assertThat(matcher.matches("1.5.0")).isTrue();
+        assertThat(matcher.matches("2.0.0")).isFalse();
+        assertThat(matcher.matches("2.0.0-redhat")).isFalse();
+        assertThat(matcher.matches("2.5.0")).isFalse();
+    }
+}
